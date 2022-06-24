24497851232138e9742807376c8865f32ad24d9b
wizard0f0s <jamie.gilmer@gmail.com>
2022-06-23 23:56:45 -0400

    Set an initial UI and added functionality to exit the app.  Staged initial growth for adding server configurations as well as the initial selection for user/group bulk management.

---
+++ src/main/java/com/wizard0f0s/tdxextended/MainController.java
@@ -0,0 +1,27 @@
+package com.wizard0f0s.tdxextended;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+    public void initialize() {
+        // More code to come ...
+    }
+
+    @FXML
+    public void showNewServerDialog() {
+        //Add server dialog code here
+    }
+
+    @FXML
+    public void bulkAssignUsersToGroups() {
+        //Bulk Assign Users to Groups here
+    }
+
+    @FXML
+    public void handleExit() {
+        Platform.exit();
+    }
+}
